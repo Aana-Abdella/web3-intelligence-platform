eeda4b775a9c8b8f9c75c59eb76f400de7a02c1e
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-25 23:49:15 +0300

    fix : nexts config to build docker

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  output: 'standalone',
+  
   outputFileTracingRoot: path.join(__dirname, '../../'),
   transpilePackages: ['@web3-intelligence/shared'],
   images: {
